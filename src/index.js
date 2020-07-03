01ea60807c99f6372dfb9a45d3645d4ac9fe9df0
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-03 14:36:59 -0700

    Nuestro primer componente

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
-// const element = document.createElement('h1')
-// element.innerText = 'Hello, React!'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Badge from './components/Badge'
 
-// const container = document.getElementById('app')
+// const element = (
+//   <div>
+//     <h1>Hola, soy Jair</h1>
+//     <p>Soy ingeniero frontend</p>
+//     <Badge />
+//   </div>
+// );
+const container = document.getElementById('app');
+const libro = document.getElementById('Hola');
 
-// container.appendChild(element);
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-// const element = <h1>Hello, React from Platzi Badges</h1>
-// const jsx = <h1>Hello, React</h1>
-
-// const element = React.createElement('a', { href: 'https://platzi.com' }, 'Ir a Platzi!')
-
-// const name = 'Jair'
-// const element = React.createElement('h1', {},`Hola, soy ${name}`)
-// const jsx = <h1>Hola me llamo, {name}</h1>
-
-const jsx = 
-<div>
-    <h1>Hola, soy Jair</h1>
-    <p>Soy ingeniero frontend</p>
-</div>;
-
-const element = React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Hola, mi nombre es Jonathan'),
-    React.createElement('p', {}, 'Soy dev frontend')
-);
-
-const container = document.getElementById('app')
-
-// ReactDOM.render(__que__, __donde__)
-// ReactDOM.render(element, container)
-ReactDOM.render(element, container)
+ReactDOM.render(<Badge />, container);
