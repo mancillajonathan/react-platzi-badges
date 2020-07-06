f596ea53cff84d87080054be3b2e86664fc70871
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-05 18:02:10 -0700

    Practica del ciclo de vida

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,50 +1,113 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import confLogo from '../images/badge-header.svg';
 import './styles/Badges.css';
 
 import BadgesList from '../components/BadgesList';
 
-
-
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-        firstName: 'Fredaaaa',
-        lastName: 'Grady',
-        email: 'Leann_Berge@gmail.com',
-        jobTitle: 'Legacy Brand Director',
-        github: 'FredaGrady37',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-      },
-      {
-        id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-        firstName: 'Major',
-        lastName: 'Rodriguez',
-        email: 'Ilene66@hotmail.com',
-        jobTitle: 'Human Research Architect',
-        github: 'ajorRodriguez15',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-      },
-      {
-        id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-        firstName: 'Daphney',
-        lastName: 'Torphy',
-        email: 'Ron61@hotmail.com',
-        jobTitle: 'National Markets Officer',
-        github: 'DaphneyT9',
-        avatarUrl:
-          'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    console.log('1. Constructor()');
+    this.state = {
+      data: [],
+      // [
+      //   {
+      //     id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+      //     firstName: 'Fredaaaa',
+      //     lastName: 'Grady',
+      //     email: 'Leann_Berge@gmail.com',
+      //     jobTitle: 'Legacy Brand Director',
+      //     github: 'FredaGrady37',
+      //     avatarUrl:
+      //       'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
+      //   },
+      //   {
+      //     id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+      //     firstName: 'Major',
+      //     lastName: 'Rodriguez',
+      //     email: 'Ilene66@hotmail.com',
+      //     jobTitle: 'Human Research Architect',
+      //     github: 'ajorRodriguez15',
+      //     avatarUrl:
+      //       'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
+      //   },
+      //   {
+      //     id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+      //     firstName: 'Daphney',
+      //     lastName: 'Torphy',
+      //     email: 'Ron61@hotmail.com',
+      //     jobTitle: 'National Markets Officer',
+      //     github: 'DaphneyT9',
+      //     avatarUrl:
+      //       'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
+      //   },
+      // ],
+    };
+  }
+
+  componentDidMount() {
+    console.log('3. ComponentDidMount()');
+
+     this.TimeoutId = setTimeout(() => {
+      this.setState({
+        data: [
+          {
+            id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+            firstName: 'Fredaaaa',
+            lastName: 'Grady',
+            email: 'Leann_Berge@gmail.com',
+            jobTitle: 'Legacy Brand Director',
+            github: 'FredaGrady37',
+            avatarUrl:
+              'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
+          },
+          {
+            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+            firstName: 'Major',
+            lastName: 'Rodriguez',
+            email: 'Ilene66@hotmail.com',
+            jobTitle: 'Human Research Architect',
+            github: 'ajorRodriguez15',
+            avatarUrl:
+              'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
+          },
+          {
+            id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+            firstName: 'Daphney',
+            lastName: 'Torphy',
+            email: 'Ron61@hotmail.com',
+            jobTitle: 'National Markets Officer',
+            github: 'DaphneyT9',
+            avatarUrl:
+              'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
+          },
+        ],
+      });
+    }, 3000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('5. componentDidUpdate()');
+    console.log({
+      prevProps: prevProps,
+      prevState: prevState,
+    });
+
+    console.log({
+      props: this.props,
+      state: this.state,
+    });
+  }
+
+  componentWillUnmount() {
+    console.log('6. componentWillUnmount');
+    clearTimeout(this.TimeoutId)
+  }
 
   render() {
+    console.log('2. Render()');
     return (
       <React.Fragment>
         <div className='Badges'>
